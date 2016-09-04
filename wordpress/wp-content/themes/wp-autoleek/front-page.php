281c0e3b3f008130e94e63089d0706b0bf9ceecd
crazyyy <crazyyy@gmail.com>
2016-09-04 13:36:14 +0300

    pools installs

--- wordpress/wp-content/themes/wp-autoleek/front-page.php
+++ wordpress/wp-content/themes/wp-autoleek/front-page.php
@@ -2,17 +2,40 @@
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-8'); ?>>
 
+      <?php query_posts("showposts=7"); ?>
+        <div class="home-carousel">
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php $show = get_field('show'); if ($show) { ?>
+              <div>
+                <?php $image = get_field('image');
+                  if( $image ) { ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                  <?php } else if ( has_post_thumbnail()) { the_post_thumbnail('slider'); } else { ?>
+                    <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                <?php } ?>
+                <h6><?php the_title(); ?></h6>
+                <a href="<?php the_permalink(); ?>" class="know-more">Узнать больше</a>
+              </div>
+            <?php } ?>
+          <?php endwhile; endif; ?>
+        </div><!-- home-carousel -->
+      <?php wp_reset_query(); ?>
+
+      <div class="subscribe-block">
+        <h6>Подпишитесь и узнайте первыми о новых публикациях на сайте<span>Мы не рассылаем спам и не передаём адреса наших подписчиков третим лицам</span></h6>
+        <form action="">
+          <input type="text" placeholder="Ваш e-mail">
+          <button>Подписка</button>
+        </form>
+      </div><!-- /.subscribe-block -->
+
+
+
       <h1 class="page-title inner-title"><?php the_title(); ?></h1>
       <?php the_content(); ?>
       <?php edit_post_link(); ?>
 
     </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article class="col-md-8">
-
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-
-    </article>
-  <?php endif; ?>
+  <?php endwhile; endif; ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/wp-autoleek/functions.php
+++ wordpress/wp-content/themes/wp-autoleek/functions.php
@@ -82,7 +82,7 @@ if (function_exists('add_theme_support')) {
   add_image_size('large', 1200, '', true); // Large Thumbnail
   add_image_size('medium', 600, '', true); // Medium Thumbnail
   add_image_size('small', 250, '', true); // Small Thumbnail
-  add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+  add_image_size('slider', 750, 370, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
   // Enables post and comment RSS feed links to head
   add_theme_support('automatic-feed-links');
